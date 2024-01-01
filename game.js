626bfa319b8a94714bd155248d20faa6edf4b21b
Syed Danyal Raza <135417798+Danyal-Raza@users.noreply.github.com>
2024-01-01 20:19:36 +0500

    game.js

--- game.js
+++ game.js
@@ -44,7 +44,7 @@ Startgame = () => {
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
         localStorage.setItem("mostRecentScore" , score);
-        return window.location.assign("end.html");
+        return window.location.assign("/end.html");
     }
 
     questionCounter++;
